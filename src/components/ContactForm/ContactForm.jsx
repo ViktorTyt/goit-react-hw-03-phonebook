944ed7f87537533149c5c6bde4624091fe2d81ec
Viktor Tytenko <92685344+ViktorTyt@users.noreply.github.com>
2022-07-20 17:23:39 +0300

    fixed method handleInputChange

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -24,7 +24,9 @@ export class ContactForm extends Component {
     this.reset();
   };
 
-  handleInputChange = (name, value) => {
+  handleInputChange = e => {
+    const { name, value } = e.currentTarget;
+
     this.setState({
       [name]: value,
     });
@@ -50,9 +52,7 @@ export class ContactForm extends Component {
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
-              onChange={({ currentTarget: { name, value } }) =>
-                this.handleInputChange(name, value)
-              }
+              onChange={this.handleInputChange}
             />
           </Label>
           <Label>
@@ -64,9 +64,7 @@ export class ContactForm extends Component {
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
-              onChange={({ currentTarget: { name, value } }) =>
-                this.handleInputChange(name, value)
-              }
+              onChange={this.handleInputChange}
             />
           </Label>
           <Button type="submit">Add contact</Button>
